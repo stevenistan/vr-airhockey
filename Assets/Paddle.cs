40406fcd311322c411e4a62f91d4b1d7b593106f
neeleshdodda44 <ndodda@berkeley.edu>
2017-10-30 17:33:43 -0700

    box collider done

---
+++ Assets/Paddle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rigidbody r = GetComponent<Rigidbody> ();
+		if (Input.GetKey ("right")) {
+			r.AddForce(10, 0, 0);
+		}
+		if (Input.GetKey ("left")) {
+			r.AddForce(-10, 0, 0);;
+		}
+		if (Input.GetKey ("up")) {
+			r.AddForce(0, 0, 10);;
+		}
+		if (Input.GetKey ("down")) {
+			r.AddForce(0, 0, -10);;
+		}
+	}
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "MyWall")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
---
+++ Assets/Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "Puck") {
+			other.GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, 0);
+			other.transform.localPosition = new Vector3 (0, 0, 0);
+			if (gameObject.name == "Wall Bottom") {
+				print ("The AI scored");
+			} else {
+				print ("You scored");
+			}
+		}
+	}
+}
